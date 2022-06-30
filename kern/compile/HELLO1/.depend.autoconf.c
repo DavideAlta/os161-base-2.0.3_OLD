255658731f4ab5778df1f56fdf504a8e3f709dbe
DavideAlta <davidealtamore12@gmail.com>
2022-06-30 19:00:48 +0200

    first os161 commit

---
+++ kern/compile/HELLO1/.depend.autoconf.c
@@ -0,0 +1,4 @@
+autoconf.o: ../../compile/HELLO1/autoconf.c ../../include/types.h \
+ ../../include/kern/types.h includelinks/kern/machine/types.h \
+ includelinks/machine/types.h ../../include/lib.h ../../include/cdefs.h \
+ opt-noasserts.h ../../compile/HELLO1/autoconf.h
